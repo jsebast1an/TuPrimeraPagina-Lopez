3ffcd33cbd07f752811ee64b090ebc1714daf88a
jsebast1an <juanselopezg05@gmail.com>
2025-04-23 20:55:45 -0500

    avances en mi chatbot

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.urls import include
+from django.shortcuts import render, redirect
+from .forms import ChatMessageForm
 
-def index(request):
-    return render(request, 'index.html')
+""" def index(request):
+    return render(request, 'index.html') """
+
+def chat_view(request):
+    if request.method == 'POST':
+        form = ChatMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')  # redirige a la misma vista (o donde quieras)
+    else:
+        form = ChatMessageForm()
+    
+    return render(request, 'index.html', {'form': form})
