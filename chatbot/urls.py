fe271e882974fd122831ad9192a56d3f983346ef
jsebast1an <juanselopezg05@gmail.com>
2025-05-05 21:19:03 -0500

    login logout profilem missing users

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.chat_view, name="index"),
+    path("", views.home_view, name="home"),
+    path('profile/', views.profile_view, name='profile'),
+    path('chat/', views.chat_view, name='chat'),
+    path('users/', views.users_view, name='users'),
+    path('about/', views.about_view, name='about'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import ChatMessageForm
-
+# views.py
 """ def index(request):
     return render(request, 'index.html') """
 
-def chat_view(request):
+def home_view(request):
     if request.method == 'POST':
         form = ChatMessageForm(request.POST)
         if form.is_valid():
@@ -14,3 +16,66 @@ def chat_view(request):
         form = ChatMessageForm()
     
     return render(request, 'index.html', {'form': form})
+
+def profile_view(request):
+
+    user = request.user  # Obtiene el usuario actual    
+    return render(request, 'profile.html', {'user': user})
+
+def chat_view(request):
+    if request.method == 'POST':
+        form = ChatMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('chat')  # redirige a la misma vista (o donde quieras)
+    else:
+        form = ChatMessageForm()
+    
+    return render(request, 'chat.html', {'form': form})
+
+def users_view(request):
+    if request.method == 'POST':
+        form = ChatMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')  # redirige a la misma vista (o donde quieras)
+    else:
+        form = ChatMessageForm()
+    
+    return render(request, 'users.html', {'form': form})
+
+def about_view(request):
+    if request.method == 'POST':
+        form = ChatMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('about')  # redirige a la misma vista (o donde quieras)
+    else:
+        form = ChatMessageForm()
+    
+    return render(request, 'about.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
